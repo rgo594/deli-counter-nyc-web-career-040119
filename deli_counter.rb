5b3df5c02adca756a38e25475ab22048430251ea
Rob O'Gara <rgo594@gmail.com>
2019-03-18 16:35:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,49 +1,25 @@
-katz_deli = ["Moshe", "Fayge", "Rivki"]
-
-
-def line(array)
-  if array.length >= 1
-    newarray = []
-    counter = 1 
-    array.each do |name|
-      newarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{newarray.join(" ")}"
-  else
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
-  end
-end
-
-line(katz_deli)
-
-def line_simple(array)
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-    current_line << " #{indexemus}. #{value},"
-  end 
-  puts current_line
-end 
-  
-line_simple(katz_deli)
-
-
-
-def take_a_number(line, new_person)
-  line.push(new_person)
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line.first}."
-    line.shift
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
 
-puts now_serving(katz_deli)
-puts katz_deli
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  end
+end
